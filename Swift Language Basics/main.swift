3bc59ff692824947cb62d6c34c5b5fdeef6e90a4
Zach Tallevast <zachtallevast@Zachs-MacBook-Pro-3.local>
2017-09-08 17:11:29 -0500

    Inital Commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Zach Tallevast on 9/8/17.
+//  Copyright © 2017 Tech Innovator. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
